8f88a2014c9253315fe48c1e29a183fa310bdf9f
sherqiye23 <serqiye.quluzade@gmail.com>
2024-12-17 16:57:26 +0400

    use state

--- REACT/startbootstrap-in-home/src/components/Header.jsx
+++ REACT/startbootstrap-in-home/src/components/Header.jsx
@@ -1,7 +1,30 @@
-import Container from 'react-bootstrap/Container'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 export default function Header() {
+    let [count, setState] = useState(0);
+    // function linkClick() {
+    //     if (dNoneNav.style.display == "none") {
+    //         dNoneNav.style.display = "flex";
+    //     } else {
+    //         dNoneNav.style.display = "none";
+    //     }
+    // }
+    function handleMinus() {
+        if (count > 0) {
+            setState(--count);
+        }
+
+    }
+    function handlePlus() {
+        setState(++count);
+
+    }
+    function handleReset() {
+        setState(0);
+        
+    }
     return (
         <>
             <header>
@@ -12,7 +35,7 @@ export default function Header() {
                             <a href="">Home</a>
                             <a href="">About</a>
                             <a href="">
-                                <span className="on-click-shop">
+                                <span className="on-click-shop" >
                                     Shop <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M11.646 15.146 5.854 9.354a.5.5 0 0 1 .353-.854h11.586a.5.5 0 0 1 .353.854l-5.793 5.792a.5.5 0 0 1-.707 0Z"></path></svg>
                                 </span>
                                 <div className="d-none-nav">
@@ -24,13 +47,16 @@ export default function Header() {
                         </nav>
                     </div>
                     <div className="card-cart-wrap">
+                        <button className="minus" onClick={handleMinus}>-</button>
                         <button className="card-cart-btn">
                             <span className="button-text">
                                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><circle cx="176" cy="416" r="32"></circle><circle cx="400" cy="416" r="32"></circle><path d="M456.8 120.78a23.92 23.92 0 0 0-18.56-8.78H133.89l-6.13-34.78A16 16 0 0 0 112 64H48a16 16 0 0 0 0 32h50.58l45.66 258.78A16 16 0 0 0 160 368h256a16 16 0 0 0 0-32H173.42l-5.64-32h241.66A24.07 24.07 0 0 0 433 284.71l28.8-144a24 24 0 0 0-5-19.93z"></path></svg>
                             </span>
                             <span className="button-text">Cart</span>
-                            <span className="button-count">0</span>
+                            <span className="button-count">{count}</span>
                         </button>
+                        <button className="plus" onClick={handlePlus}>+</button>
+                        <button className="reset" onClick={handleReset}>Reset</button>
                     </div>
                 </Container>
             </header>
--- REACT/startbootstrap-in-home/src/components/Main.jsx
+++ REACT/startbootstrap-in-home/src/components/Main.jsx
@@ -1,12 +1,12 @@
 import Hero from './main sections/Hero'
-// import Cards from './main sections/Cards'
+import Cards from './main sections/Cards'
 
 
 export default function Main() {
     return (
         <>
             <Hero/>
-            {/* <Cards/> */}
+            <Cards/>
         </>
     )
 }
