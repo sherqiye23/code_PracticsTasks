fa40d4e882b65ef4717ea2816e42f8f1e10ccfdc
sherqiye23 <serqiye.quluzade@gmail.com>
2024-12-18 18:32:02 +0400

    form

--- REACT/form-events/src/App.jsx
+++ REACT/form-events/src/App.jsx
@@ -3,9 +3,17 @@ import './App.css';
 import categories from './categories';
 
 function App() {
+  let [font, setFont] = useState(16)
+
+  let [ipnutValue, setipnutValue] = useState("");
+
+  const cloneCategories = [...categories];
   let [myCategories, setMyCategories] = useState(categories)
   let [inputName, setInputName] = useState("");
   let [inputDesc, setInputDesc] = useState("");
+  let [inputPrice, setInputPrice] = useState("");
+
+  let [btnText, setInputBtnText] = useState("Hide");
 
   // add function
   function addCategory(e) {
@@ -17,24 +25,84 @@ function App() {
     let newCategory = {
       name: inputName.trim(),
       description: inputDesc.trim(),
+      price: inputPrice,
       id: self.crypto.randomUUID()
     }
     setMyCategories([...myCategories, newCategory]);
+    setInputName("")
+    setInputDesc("")
+    setInputPrice("")
   }
   // delete function
   function deleteCategory(id) {
     let filteredCategories = myCategories.filter(myCategory => myCategory.id != id)
     setMyCategories(filteredCategories)
   }
+  // sort
+  function sortCategory() {
+    let sortedCategories = myCategories.toSorted((a, b) => a.price - b.price)
+    if (JSON.stringify(sortedCategories) == JSON.stringify(myCategories)) {
+      setMyCategories(cloneCategories)
+    } else {
+      setMyCategories(sortedCategories);
+    }
+  }
+  // clear
+  function clearCategory() {
+    if (JSON.stringify(myCategories) == JSON.stringify([])) {
+      setMyCategories(cloneCategories);
+      setInputBtnText("Hide");
+      return
+    }
+    setMyCategories([]);
+    setInputBtnText("Show");
+  }
+
+  // search
+  function searchCategories(e) {
+    let searchArray = categories.filter(myCategory => {
+      let categoryName = myCategory.name.toLocaleLowerCase();
+      let inputValue = e.target.value.toLocaleLowerCase()
+      return (
+        categoryName.startsWith(inputValue)
+      )
+    })
+    console.log(searchArray);
+    setMyCategories(searchArray)
+
+  }
+
+  function ipnutFunction(e) {
+    setipnutValue(e.target.value)
+  }
+
+  function increaseF() {
+    if (font < 10) {
+      return
+    }
+    setFont(font-1)
+  }
   return (
     <>
       <div className="container">
+      <p style={{fontSize:font+"px", margin:"5px 10px 0px"}}>Text i you he she it we you they</p>
+      <button type="button" className='reset-btn' onClick={() => increaseF() }>Increase</button>  
+      <button type="button" className='reset-btn' onClick={() => { setFont(font+1) }}>Decrease</button>  
+      <button type="button" className='reset-btn' onClick={() => { setFont(16) }}>Reset</button>  
+      <hr />
+        <input type="text" className='ipnut' value={ipnutValue} placeholder='...text' onChange={(e) => ipnutFunction(e)} />
+        <p style={{fontSize:"20px", margin:"5px 10px 0px"}}>Text - {ipnutValue}</p>
+        <button type="button" className='reset-btn' onClick={() => { setipnutValue("") }}>Reset</button>  
         <div className="form-wrapper">
           <form onSubmit={(e) => addCategory(e)}>
-            <input type="text" className='name' placeholder='Name' onChange={(e) => setInputName(e.target.value)} />
-            <input type="text" className='description' placeholder='Description' onChange={(e) => setInputDesc(e.target.value)} />
+            <input type="text" required className='name' value={inputName} placeholder='Name' onChange={(e) => setInputName(e.target.value)} />
+            <input type="text" required className='description' value={inputDesc} placeholder='Description' onChange={(e) => setInputDesc(e.target.value)} />
+            <input type="number" required className='price' value={inputPrice} placeholder='Price' onChange={(e) => setInputPrice(e.target.value)} />
             <button type="submit" className='form-btn'>Add</button>
           </form>
+          <button type="button" className='sort-btn' onClick={() => { sortCategory() }}>Sort by price</button>
+          <button type="button" className='clear-btn' onClick={() => { clearCategory() }}>{btnText} All</button>
+          <input type="text" className='search-input' placeholder='Search' onChange={(e) => searchCategories(e)} />
         </div>
         <div className="categories-cards">
           {
@@ -43,6 +111,7 @@ function App() {
                 <details>
                   <summary><h3 style={{ display: "inline-block" }}>{myCategory.name}</h3></summary>
                   <p>{myCategory.description}</p>
+                  <p>{myCategory.price}$</p>
                 </details>
                 <button className='delete-btn' onClick={() => deleteCategory(myCategory.id)}>Delete</button>
               </div>
--- REACT/form-events/src/categories.js
+++ REACT/form-events/src/categories.js
@@ -2,42 +2,50 @@ let categories = [
     {
         "id": 1,
         "description": "Soft drinks coffees teas beers and ales",
-        "name": "Beverages"
+        "name": "Beverages",
+        "price": 20
     },
     {
         "id": 2,
         "description": "Sweet and savory sauces relishes spreads and seasonings",
-        "name": "Condiments"
+        "name": "Condiments",
+        "price": 40
     },
     {
         "id": 3,
         "description": "Desserts candies and sweet breads",
-        "name": "Confections"
+        "name": "Confections",
+        "price": 120
     },
     {
         "id": 4,
         "description": "Cheeses",
-        "name": "Dairy Products"
+        "name": "Dairy Products",
+        "price": 230
     },
     {
         "id": 5,
         "description": "Breads crackers pasta and cereal",
-        "name": "Grains/Cereals"
+        "name": "Grains/Cereals",
+        "price": 90
     },
     {
         "id": 6,
         "description": "Prepared meats",
-        "name": "Meat/Poultry"
+        "name": "Meat/Poultry",
+        "price": 10
     },
     {
         "id": 7,
         "description": "Dried fruit and bean curd",
-        "name": "Produce"
+        "name": "Produce",
+        "price": 25
     },
     {
         "id": 8,
         "description": "Seaweed and fish",
-        "name": "Seafood"
+        "name": "Seafood",
+        "price": 2
     }
 ]
 
